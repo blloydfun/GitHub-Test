91bfca78d7ce34d2ef4038bde853d744079133cf
Brett Lloyd <36777241+blloydfun@users.noreply.github.com>
2018-03-05 13:08:42 -0800

    Not sure what

--- Github-Test/Program.cs
+++ Github-Test/Program.cs
@@ -11,6 +11,11 @@ namespace Github_Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("this is a test");
+            Console.WriteLine("this is another test");
+            Console.WriteLine("does it always just add to master?");
+            Console.WriteLine("more additions");
+            Console.WriteLine("just checked it out");
             Console.ReadLine();
         }
     }
